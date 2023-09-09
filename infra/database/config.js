497ffa30ccdcfdb22bef141cdfa29e4827cd4550
Victor <victorhcardinali@gmail.com>
2023-09-09 18:01:48 -0300

    build(database): corrige e configura banco de dados para produção

--- infra/database/config.js
+++ infra/database/config.js
@@ -1,24 +1,14 @@
 require("dotenv").config();
 
-function getDatabaseName() {
-  const mainName = `cuja_${process.env.NEXT_PUBLIC_VERCEL_ENV}`;
-  const isPreviewEnv = process.env.NEXT_PUBLIC_VERCEL_ENV === "preview";
-  const database = isPreviewEnv
-    ? `${mainName}_${process.env.VERCEL_GIT_PULL_REQUEST_ID}`
-    : mainName;
-  return database;
-}
-
 module.exports = {
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: getDatabaseName(),
+  database: process.env.MYSQL_DATABASE,
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
   dialect: "mysql",
   dialectOptions: {
     ssl: {
-      ca: "infra/database/ca.crt",
       rejectUnauthorized: false,
     },
   },
