41cb342b1ff55fcd50cc38dbc16f93c20ba0b5cc
Victor Henrique Cardinali Barros <85972147+victorhcb@users.noreply.github.com>
2023-11-15 02:52:11 -0300

    refactor(database): troca banco de dados para postgres (#35)
    * refactor(database): remove sequelize e conexões com db
    
    remove sequelize com mysql para subistituir pelo pg com pgbouncer
    
    * refactor(database): adiciona banco de dados postgres
    
    adiciona banco de dados postgres, node-pg, migrations e docker postgres
    
    #2

--- src/app/status/route.js
+++ src/app/status/route.js
@@ -1,10 +1,7 @@
 import { NextResponse } from "next/server";
 
-import database from "infra/db/database.mjs";
-
 export async function GET(request) {
-  const result = await database.query("SELECT 1+1;");
   return NextResponse.json({
-    message: "Conexão com banco de dados estabelecida com sucesso.",
+    message: "OK",
   });
 }
