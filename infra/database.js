5840a1a6cd974e16d296c5b78adae116eb7142b4
Victor Henrique Cardinali Barros <85972147+victorhcb@users.noreply.github.com>
2023-11-15 02:50:19 -0300

    refactor(database): troca banco de dados para postgres
    * refactor(database): remove sequelize e conexões com db
    
    remove sequelize com mysql para subistituir pelo pg com pgbouncer
    
    * refactor(database): adiciona banco de dados postgres
    
    adiciona banco de dados postgres, node-pg, migrations e docker postgres
    
    #2

---
+++ infra/database.js
@@ -0,0 +1,58 @@
+import migrationRunner from "node-pg-migrate";
+import { Pool } from "pg";
+
+const configurations = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+  },
+};
+
+if (!process.env.VERCEL_ENV) {
+  delete configurations.ssl;
+}
+
+const pool = new Pool(configurations);
+
+async function query(queryObject) {
+  const client = await pool.connect();
+  try {
+    return await client.query(queryObject);
+  } finally {
+    client.release();
+  }
+}
+
+async function downAllMigrations() {
+  const client = await pool.connect();
+  try {
+    return await migrationRunner({
+      dir: "./infra/migrations",
+      direction: "down",
+      dbClient: client,
+      migrationsTable: "pgmigrations",
+    });
+  } finally {
+    client.release();
+  }
+}
+
+async function runAllMigrations() {
+  const client = await pool.connect();
+  try {
+    return await migrationRunner({
+      dir: "./infra/migrations",
+      direction: "up",
+      dbClient: client,
+      migrationsTable: "pgmigrations",
+    });
+  } finally {
+    client.release();
+  }
+}
+
+export default Object.freeze({
+  query,
+  runAllMigrations,
+  downAllMigrations,
+});
--- tests/orchestration/orchestrator.js
+++ tests/orchestration/orchestrator.js
@@ -1,5 +1,3 @@
-import database from "infra/db/database.mjs";
-
 const isServerlessRuntime = !!process.env.NEXT_PUBLIC_VERCEL_ENV;
 const isProduction = process.env.NEXT_PUBLIC_VERCEL_ENV === "production";
 
@@ -9,17 +7,6 @@ const host = isProduction
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : `http://${process.env.NEXT_PUBLIC_WEBSERVER_HOST}:${process.env.NEXT_PUBLIC_WEBSERVER_PORT}`;
 
-async function refreshDatabase() {
-  await database.migrator.down({ to: 0 });
-  await database.migrator.up();
-}
-
-async function closeDatabaseConnections() {
-  await database.close();
-}
-
 export default Object.freeze({
   host,
-  refreshDatabase,
-  closeDatabaseConnections,
 });
