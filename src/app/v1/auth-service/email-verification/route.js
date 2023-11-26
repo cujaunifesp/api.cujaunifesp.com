279c110a3fd006dd005816e872fde78e5f714f97
Victor <dticuja@gmail.com>
2023-11-26 20:48:56 -0300

    feat(email): adiciona validação de email na verificação de email
    adiciona validação e suas respectivas mensagens de erro para melhorar a informação de erro para o
    usuário

--- src/app/v1/auth-service/email-verification/route.js
+++ src/app/v1/auth-service/email-verification/route.js
@@ -1,14 +1,21 @@
 import emailVerificationService from "src/services/auth/email-verification";
 import controller from "utils/controller";
+import validator from "utils/validator";
 
 export async function POST(request) {
   try {
     const requestBody = await request.json();
-    await emailVerificationService.startEmailVerification(requestBody.email);
 
-    return controller.response.ok(201, {
-      message: "Código de verificação enviado com sucesso",
+    const secure = validator.run(requestBody, {
+      email: {
+        required: true,
+        type: validator.types.EMAIL,
+      },
     });
+
+    await emailVerificationService.startEmailVerification(secure.email);
+
+    return controller.response.ok(201, {});
   } catch (error) {
     return controller.response.error(error);
   }
