330083c3082ab92192041bbb2863ea58c54fd842
CUJA UNIFESP <145414487+cujaunifesp@users.noreply.github.com>
2023-11-26 15:21:40 -0300

    Revert "fix(cors): adiciona configurações de cors no `vercel.json` (#63)"
    This reverts commit 590747f8d35669eac98d15821c1b7f745e2b6dd8.

--- next.config.js
+++ next.config.js
@@ -2,4 +2,29 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      // ENABLES CORS
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "*",
+          },
+        ],
+      },
+    ];
+  },
 };
