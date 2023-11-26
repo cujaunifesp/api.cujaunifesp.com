b1d0a88d66708a1c0c01e92465cde64808e2349b
CUJA UNIFESP <145414487+cujaunifesp@users.noreply.github.com>
2023-11-26 15:37:49 -0300

    feat(cors): cria e corrige o middleware.js

---
+++ src/middleware.js
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(request) {
+  if (request.method === "OPTIONS") {
+    return NextResponse.json("ok", {status: 200});
+  }
+
+  return NextResponse.next();
+}
