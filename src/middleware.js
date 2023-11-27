939e9c5caab888288e96827d390b00ae0d335c93
CUJA UNIFESP <145414487+cujaunifesp@users.noreply.github.com>
2023-11-27 00:43:55 -0300

    feat(token): adiciona permissões para `token` com method `root` (#65)
    * feat(token): adiciona permissões para `token` com method `root`
    
    * style(lint): corrige erros acusados pelo ci do lint

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,8 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
 export function middleware(request) {
   if (request.method === "OPTIONS") {
-    return NextResponse.json("ok", {status: 200});
+    return NextResponse.json("ok", { status: 200 });
   }
 
   return NextResponse.next();
