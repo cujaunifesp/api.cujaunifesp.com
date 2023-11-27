45188510bc5253f1ce3686c22852a461c327c5d9
Victor <dticuja@gmail.com>
2023-11-27 00:41:47 -0300

    style(lint): corrige erros acusados pelo ci do lint

--- next.config.js
+++ next.config.js
@@ -20,8 +20,7 @@ module.exports = {
           },
           {
             key: "Access-Control-Allow-Headers",
-            value:
-              "*",
+            value: "*",
           },
         ],
       },
